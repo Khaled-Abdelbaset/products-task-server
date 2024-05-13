738940d77fc9d7cd54ab95acc529570d1aa52596
Khaled Abdelbaset <Khaledabdelbaset98@gmail.com>
2024-05-13 11:23:58 +0300

    updating config file

--- Config/DB.php
+++ Config/DB.php
@@ -7,3 +7,13 @@
     "username"=> "root",
     "password"=> "",
   ];
+
+  // Server
+  // return [
+  //   "host"=> "localhost",
+  //   "port"=> "",
+  //   "dbname"=> "id22159605_products_task",
+  //   "dbdriver" => "mysql",
+  //   "username"=> "id22159605_products_task",
+  //   "password"=> "Scandiweb@2024",
+  // ];
